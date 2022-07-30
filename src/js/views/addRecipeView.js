e9b973787792ba48d3d745d77cb473affec3b2b7
Abdul Samad <abdulsamad.laghari1@gmail.com>
2022-07-30 14:36:56 +0500

    update

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,26 +1,10 @@
 import View from './View';
 import icons from 'url:../../img/icons.svg';
-import { bind } from 'core-js/core/function';
-
 class AddRecipeView extends View {
-  _parentElement = document.querySelector('.upload');
-  _window = document.querySelector('.add-recipe.window');
-  _overlay = document.querySelector('.overlay');
-  _btnOpen = document.queryCommandValue('.nav__btn--add-recipe');
-  _btnClose = document.queryCommandValue('.btn--close-modal');
+  _parentElement = document.querySelector('.pagination');
 
-  constructor() {
-    super();
-    this._addHandlerShowWindow();
-  }
-
-  toggleWindow() {
-    this._overlay.classList.toggle('hidden');
-    this._window.classList.toggle('hidden');
-  }
-
-  _addHandlerShowWindow() {
-    this._btnOpen.addEventListener('click', this.toggleWindow, bind(this));
+  _generateMarkup() {
+    return '';
   }
 }
 
